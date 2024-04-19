3fc797eb70641c3c001feeeece47c6afe8ab6cab
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-04-19 16:25:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// let NumPrompt = +prompt('Raqam kiriting')
+
+// if (NumPrompt % 2 == 0) {
+//     console.log('even');
+// }
+
+
+// else if (NumPrompt % 1 == 0) {
+//     console.log('odd');
+// }
+
+
+
+// let NumberPrompt = +prompt('Raqam kiriting')
+
+// if (NumberPrompt % 4 == 0) {
+//     console.log('Kabisa yili');
+// }
+
+
+// else {
+//     console.log('Kabisa yili emas');
+// }
+
+
+
+// let MevaPrompt = prompt('Meva nomini kiriting')
+
+// if (MevaPrompt == 'olma') {
+//     console.log('10.000 som');
+// }
+
+
+// else if (MevaPrompt == 'nok') {
+//     console.log('12.000 som');
+// }
+
+
+// else if (MevaPrompt == 'tarvuz') {
+//     console.log('20.000 som');
+// }
+
+
+// else {
+//     console.log('Unday meva mavjud emas!');
+// }
+
+
+
+let NumPrompt5 = +prompt('Istalgan sonni kiriting')
+
+if (NumPrompt5 % 3 == 0) {
+    console.log('Fizz');
+}
+
+else if (NumPrompt5 % 5 == 0) {
+    console.log('Buzz');
+}
+
+
+else if (NumPrompt5 % 3 == 0 && NumPrompt5 % 5 == 0) {
+    console.log('FizzBuzz');
+}
